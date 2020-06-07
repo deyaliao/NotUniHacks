234ba04bb13bfcfbd74e4afdca9bd9f35cc4165d
maggieguan149 <64670344+maggieguan149@users.noreply.github.com>
2020-06-07 04:53:32 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -12,28 +12,12 @@ firebase.auth().onAuthStateChanged(function(user) {
 
       }
           
-         initChat(user);
-
-          function initChat(user) {
-            // Get a Firebase Database ref
-            var chatRef = firebase.database().ref("chat");
-    
-            // Create a Firechat instance
-            var chat = new FirechatUI(chatRef, document.getElementById("chat-wrapper"));
-    
-            // Set the Firechat user
-            chat.setUser(user.uid, user.displayName);
-        }
     } else {
       // No user is signed in.
       document.getElementById("user_page").style.display = "none";
       document.getElementById("login_page").style.display = "block";
     }
   });
-
-//   firebase.auth().signInWithCustomToken(<CAAAAVocUx3Q:APA91bEx06x-3DR8vqYScSLVT8zEF54eQt-3OiCkt9hSvRoFyRaef9-TJ6-EZH3kzVj5zOoaxT2Qz9UumXZOiw3wR-btA8K-1sqaNX8pQJOgmm49qy1C3IMY4hGkurnNtbtpnMH7JDaY>).catch(function(error) {
-//     console.log("Error authenticating user:", error);
-//   });
 
 function login(){
     
@@ -47,12 +31,6 @@ function login(){
         
         window.alert("Error: " + errorMessage);
       });
-
-      // <script type="text/javascript">
-      //     document.getElementById("test").onclick = function(){
-      //         window.location.href='/Users/deyaliao/hack1/public/pages/dashboard1_0.html'
-      //             }
-      //   </script>
   }
 
 
